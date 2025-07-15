f7deb7a6d5e4abcebc6b3620ca6032a6cbbb02b7
Eyob Tesfaye <eyobimpactinglife@gmail.com>
2025-07-15 21:25:40 +0300

    modifiedApp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,24 @@ import NotFound from './pages/NotFound';
 import JobPage , {JobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
+
+
+const App = () => {
+  const addJob = async (newJob) => {
+  console.log(newJob);
+}
+
 const router  = createBrowserRouter (
   createRoutesFromElements (
     <Route path="/" element = {<MainLayout/>}>
     <Route index element =  {<HomePages/>}/> 
     <Route path = "/jobs/:id" element =  {<JobPage/>} loader = {JobLoader}/> 
     <Route path = "/jobs" element =  {<JobsPage/>}/> 
-    < Route path = "/add-job" element =  {<AddJobPage/>}/> 
+    < Route path = "/add-job" element =  {<AddJobPage addjobssubmit = {addJob}/>}/> 
     <Route path = "*" element =  {<NotFound/>}/> 
 
     </Route>
   ) ) ; 
-
-const App = () => {
   return <RouterProvider router = {router}/> ; 
   
 
